9c44fbadaa7da763afe4808c9b9cb47e5eb2173a
Jeimel <jablinskifelix@gmail.com>
2024-02-23 22:26:18 +0100

    clean css, add project section

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,61 @@
 mod perlin;
 
 use web_sys::window;
-use yew::{classes, html, Component, Context, Html};
+use yew::{classes, function_component, html, Component, Context, Html, Properties};
+
+#[derive(Properties, PartialEq)]
+struct HeaderProperties {
+    pub text: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct ButtonProperties {
+    pub title: String,
+    pub link: String,
+    #[prop_or(true)]
+    pub new_tab: bool,
+}
+
+#[derive(Properties, PartialEq)]
+struct ProjectProperties {
+    pub title: String,
+    pub link: String,
+    pub description: String,
+}
+
+#[function_component]
+fn Header(props: &HeaderProperties) -> Html {
+    html! {
+        <h1 class={classes!("header")}>
+            {props.text.clone()}
+        </h1>
+    }
+}
+
+#[function_component]
+fn Button(props: &ButtonProperties) -> Html {
+    html! {
+        <span class={classes!("button")}>
+            if props.new_tab {
+                <a target="_blank" rel="noopener noreferrer" href={props.link.clone()}>{props.title.clone()}</a>
+            } else {
+                <a href={props.link.clone()}>{props.title.clone()}</a>
+            }
+        </span>
+    }
+}
+
+#[function_component]
+fn Project(props: &ProjectProperties) -> Html {
+    html! {
+        <div class={classes!("project")}>
+            <Button title={props.title.clone()} link={props.link.clone()}/>
+            <span class={classes!("project-text")}>
+                {props.description.clone()}
+            </span>
+        </div>
+    }
+}
 
 struct Index;
 
@@ -33,7 +87,7 @@ impl Component for Index {
                 (16, 9, 40)
             } else {
                 (9, 20, 30)
-            }
+            };
         }
 
         let perlin_base64 =
@@ -41,23 +95,22 @@ impl Component for Index {
 
         html! {
             <main>
-                <section id="front" class={classes!("front-section")} style={format!("background-image: url(data:image/jpeg;base64,{})", perlin_base64)}>
-                    <h1 class={classes!("header-message")}>{"hi, i'm felix."}</h1>
+                <section id="front" class={classes!("section", "front-background")} style={format!("background-image: url(data:image/jpeg;base64,{})", perlin_base64)}>
+                    <Header text={"hi, i'm felix."} />
                     <div class={classes!("front-container")}>
-                        <span class={classes!("front-button")}>
-                            <a href="https://github.com/jeimel">
-                                {"github"}
-                            </a>
-                        </span>
-                        <span class={classes!("front-button")}>
-                            <a href="#projects">
-                                {"projects"}
-                            </a>
-                        </span>
+                        <Button title={"github"} link={"https://github.com/jeimel"} />
+                        <Button title={"projects"} link={"#projects"} new_tab={false} />
                     </div>
                 </section>
-                <section id="projects" class={classes!("projects-section")}>
-                <h1 class={classes!("header-message")}>{"projects"}</h1>
+                <section id="projects" class={classes!("section")}>
+                    <Header text={"projects."} />
+                    <div class={classes!("")}>
+                        <Project title={"jeimel.github.io"} link={"https://github.com/Jeimel/jeimel.github.io"} description={"Access the source code for this website, inclusive of a Perlin Noise implementation employed for procedural background image generation. All future subpages will be included in the repository."} />
+                        <Project title={"infinite-craft"} link={"https://github.com/Jeimel/infinite-craft"} description={"Currently a Python command line tool to fetch all the recipes and elements from Neal's infinte-craft."} />
+                    </div>
+                </section>
+                <section id="footer" class={classes!("section")}>
+                    <Header text={"jeimel."} />
                 </section>
             </main>
         }
