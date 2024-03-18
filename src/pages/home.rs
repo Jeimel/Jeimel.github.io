4d6c9f4fc34b94d6061348d0638d04a4666c11d8
Felix Jablinski <96843546+Jeimel@users.noreply.github.com>
2024-03-18 21:23:12 +0100

    Change background (#6)

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -3,10 +3,35 @@ use yew::{classes, html, Component, Context, Html};
 
 use crate::components::{perlin::Terrain, project::Project, ui::Button, ui::Header};
 
-const BACKGROUND_WIDTH: [u32; 3] = [16 * 40, 9 * 30, 9 * 35];
-const BACKGROUND_HEIGHT: [u32; 3] = [9 * 40, 20 * 30, 16 * 35];
-const FRONT_WIDTH: [u32; 3] = [800, 350, 550];
-const FRONT_HEIGHT: [u32; 3] = [250, 120, 80];
+trait Background {
+    fn width(&self) -> u32;
+
+    fn height(&self) -> u32;
+}
+
+struct Mobile;
+
+impl Background for Mobile {
+    fn width(&self) -> u32 {
+        9 * 30
+    }
+
+    fn height(&self) -> u32 {
+        20 * 30
+    }
+}
+
+struct Desktop;
+
+impl Background for Desktop {
+    fn width(&self) -> u32 {
+        16 * 40
+    }
+
+    fn height(&self) -> u32 {
+        9 * 40
+    }
+}
 
 pub struct Home;
 
@@ -20,8 +45,7 @@ impl Component for Home {
     }
 
     fn view(&self, _: &Context<Self>) -> Html {
-        let (mut index, mut front_ratio, mut window_width, mut window_height, mut mobile) =
-            (0, 0f32, 1920, 1080, false);
+        let (mut window_width, mut window_height, mut mobile) = (1920, 1080, false);
 
         if let Some(window) = window() {
             window_width = window
@@ -35,34 +59,27 @@ impl Component for Home {
                 .as_f64()
                 .unwrap_or(0f64) as u32;
 
-            if window_width < 700 {
-                index = 1;
-                front_ratio = FRONT_HEIGHT[index] as f32 / window_width as f32;
-            } else if window_width < 1120 {
-                index = 2;
-                front_ratio = FRONT_WIDTH[index] as f32 / window_width as f32 - 0.35f32;
-            } else {
-                front_ratio = FRONT_WIDTH[index] as f32 / window_width as f32 + 0.125f32;
-            }
-
             mobile = window_width < 1120;
         }
+
+        let background: Box<dyn Background> = if window_width < 700 {
+            Box::new(Mobile {})
+        } else if window_width < 1120 {
+            Box::new(Desktop {})
+        } else {
+            Box::new(Desktop {})
+        };
 
         html! {
             <main>
                 <Terrain
-                    width={BACKGROUND_WIDTH[index]}
-                    height={BACKGROUND_HEIGHT[index]}
-                    front_ratio={front_ratio}
+                    width={background.width()}
+                    height={background.height()}
                     window_width={window_width}
                     window_height={window_height}
                     mobile={mobile}
                 >
-                    <div
-                        style={
-                            format!("width: {}px; height: {}px", FRONT_WIDTH[index], FRONT_HEIGHT[index])
-                        }
-                    >
+                    <div>
                         <Header text={"hi, i'm felix."} />
                         <div class={classes!("button-container")}>
                             <Button title={"github"} link={"https://github.com/jeimel"} />
@@ -77,10 +94,16 @@ impl Component for Home {
                             title={"jeimel.github.io"}
                             link={"https://github.com/Jeimel/jeimel.github.io"}
                             description={
-                                "Access the source code for this website, inclusive
-                                of a Perlin Noise implementation employed for procedural 
-                                background image generation. All future subpages will be 
-                                included in the repository."
+                                ""
+                            }
+                        />
+                    </div>
+                    <div>
+                        <Project
+                            title={"Battleship"}
+                            link={"https://github.com/Jeimel/Battleship"}
+                            description={
+                                ""
                             }
                         />
                     </div>
